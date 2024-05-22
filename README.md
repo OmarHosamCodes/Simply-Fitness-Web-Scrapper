62e08005c72c1352764db8105d98d2f6e76496df
omarhosamcodes <omarhosamcodes@gmail.com>
2024-05-22 05:41:53 +0300

    non blocking faster V2

--- README.md
+++ README.md
@@ -3,25 +3,29 @@
 ### Get Started
 
 ### Clone
-###### With Github CLI 
+
+###### With Github CLI
+
 ```
 gh repo clone OmarHosamCodes/Simply-Fitness-Web-Scrapper
-
 ```
+
 ###### With Git
+
 ```
 git clone https://github.com/OmarHosamCodes/Simply-Fitness-Web-Scrapper.git
-
 ```
+
 ### Use
+
 #### Get To The Repository
-``` 
-cd Simply-Fitness-Web-Scrapper/
 
 ```
+cd Simply-Fitness-Web-Scrapper/
+```
+
 #### Run main.py
+
 ```
 python main.py
-
 ```
-
--- main.py
+++ main.py
@@ -1,70 +1,92 @@
-import random
-import requests
-from bs4 import BeautifulSoup
 import os
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
 
-def fetchListContent(parentURL, listContent, names):
-    response = requests.get(parentURL)
-    soup = BeautifulSoup(response.content, "html.parser")
-    targetClass = soup.find_all(class_="exo-ol no-bullets site-footer__linklist")
-    for element in targetClass:
-        targetListItems = element.find_all("li")
-        for item in targetListItems:
-            targetListItemLinks = item.find_all("a")
-            for link in targetListItemLinks:
-                href = link["href"]
-                listContent.append(f"https://www.simplyfitness.com{href}")
-                names.append(href.strip("/pages/"))
-    finalListContent = list(set(listContent))
-    print("The list of content has been fetched")
 
-def fixURLs(src):
-    if src.startswith("//"):
-        return f"https:{src}"
-    else:
-        return src
-
-def fetchContent(listContent, contentURLs):
-    for contentURL in listContent:
-        response = requests.get(contentURL)
-        soup = BeautifulSoup(response.content, "html.parser")
-        targetClass = soup.find_all("header")
-        for _ in targetClass:
-            targetDiv = _.find_all("div")
-            for div in targetDiv:
-                targetImg = div.find_all("img")
-                for img in targetImg:
-                    finalURL = fixURLs(img["src"])
-                    print(finalURL)
-                    contentURLs.append(finalURL)
-    finalContent = list(set(contentURLs))
-    print("The content has been fetched")
-  
-def createDirectory():
-    os.mkdir('workouts')
-
-def downloadImages( content):
-    os.chdir('workouts')
-    for image in content:
-        response = requests.get(image)
-        random_number = random.randint(1, 9999999)
-        with open(f"image_{random_number}.png", "wb") as f:
-            f.write(response.content)
-    os.chdir("..")
-    print("The images have been downloaded")
-
-def main():
+class SimplyFitnessWebScrapper:
     parentURL = "https://www.simplyfitness.com/pages/workout-exercise-guides"
-    listContent = []
-    names = []
-    contentURLs = []
-    fetchListContent(parentURL, listContent, names)
-    createDirectory()
-    fetchContent(listContent, contentURLs)
-    listContent = list(set(listContent))
-    contentURLs = list(set(contentURLs))
-    downloadImages(contentURLs)
-    print("The program has finished running")
+
+    async def get_list_content(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.parentURL) as response:
+                text = await response.text()
+                document = BeautifulSoup(text, "html.parser")
+                content_and_name = set()
+
+                target_class = document.select(
+                    ".exo-ol.no-bullets.site-footer__linklist li a"
+                )
+
+                for element in target_class:
+                    href = element.get("href")
+                    content_and_name.add(
+                        (
+                            f"https://www.simplyfitness.com{href}",
+                            href.replace("/pages/", "") if href else "",
+                        )
+                    )
+
+                print(f"The list of content has been fetched: {content_and_name}")
+                return content_and_name
+
+    def fix_urls(self, src):
+        return f"https:{src}" if src.startswith("//") else src
+
+    async def get_content(self, list_content):
+        content_urls_and_names = set()
+
+        async with aiohttp.ClientSession() as session:
+            for content_url, name in list_content:
+                async with session.get(content_url) as response:
+                    text = await response.text()
+                    document = BeautifulSoup(text, "html.parser")
+
+                    target_class = document.select("header div img")
+
+                    for element in target_class:
+                        final_url = self.fix_urls(element.get("src", ""))
+                        print(final_url)
+                        content_urls_and_names.add((final_url, name))
+
+        print(f"The content has been fetched: {content_urls_and_names}")
+        return content_urls_and_names
+
+    async def create_folders(self):
+        os.makedirs("workouts", exist_ok=True)
+
+    async def download_images(self, content):
+        os.chdir("workouts")
+        list_of_content = list(content)
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for final_url, name in list_of_content:
+                tasks.append(self.download_image(session, final_url, name))
+            await asyncio.gather(*tasks)
+        print("The images have been downloaded")
+
+    async def download_image(self, session, url, name):
+        async with session.get(url) as response:
+            content = await response.read()
+            file_name = f"{name}.png"
+            with open(file_name, "wb") as file:
+                file.write(content)
+            print(f"Downloaded {file_name}")
+
+
+async def main():
+    scrapper = SimplyFitnessWebScrapper()
+    await scrapper.create_folders()
+    content = await scrapper.get_list_content()
+    content_urls_and_names = await scrapper.get_content(content)
+    await scrapper.download_images(content_urls_and_names)
+    print("The process has been completed")
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except aiohttp.ServerDisconnectedError:
+        exit("Script Ended")
+    except:
+        exit("Script Ended")
